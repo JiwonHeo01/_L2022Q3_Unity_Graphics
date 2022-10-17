87aed236b7eb2c7daea561a6847b88b210ef9cc6
JiwonHeo <90255456+JiwonHeo01@users.noreply.github.com>
2022-10-17 14:50:39 +0900

    22.10.17_W6

---
+++ Assets/week06A_Light/Light_Controller.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Light_Controller : MonoBehaviour
+{
+    public GameObject Light;
+    bool isOn;
+    void Start()
+    {
+        isOn = false;
+        Light.SetActive(isOn);
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.L))
+        {
+            print("L down");
+            print(Light.name);
+            isOn = !isOn;
+            Light.SetActive(isOn);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        print(gameObject.name + "mouse down");
+    }
+}
